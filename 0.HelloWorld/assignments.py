c105158e189483f545730d0822d4b5bb35b4354c
pradeepkarki <pradeepkarki70@gmail.com>
2021-10-31 22:59:12 +0530

    varables and datatypes

---
+++ 0.HelloWorld/assignments.py
@@ -0,0 +1,7 @@
+# 1.use pip install to install any module
+
+# 2.program to print the contents of the OS dir
+
+import os
+
+print(os.listdir('.'))
---
+++ 1.variablesDatatypes/VariablesDatatypes.py
@@ -0,0 +1,21 @@
+#String
+a = 'string'
+a1 = '''long 
+string'''
+print(a1)
+#integer
+b = 100
+print(b)
+#Float
+c = 10.2
+print(c)
+
+#boolean
+d = True
+
+#none
+
+e = None
+
+# print type
+
